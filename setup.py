e3883a808316ab9752be7f7e9cdc1294dab85694
moeabench-rgb <moeabench@gmail.com>
2025-10-29 20:46:47 -0300

    sdf

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 from setuptools import setup, find_packages
 
 
-setup (
-
+setup(
+    name="my_implementations",
+    version="0.1",
     packages=find_packages(),
-    install_requires = open("requirements.txt").read().splitlines(),
-
-
 )
